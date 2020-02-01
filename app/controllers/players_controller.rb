92f7b72100b1c385feba45334682643b4d1aff79
apa yu <rx836@hotmail.com>
2020-02-01 09:46:58 +0800

    調整table 樣式

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,15 @@
 class PlayersController < ApplicationController
   def index
     @players = Player.all
+    @stats = Stat.all
+    @point_sum =  @stats.where('points >= 0').sum("points")
+    @tpm_sum =  @stats.where('tpm >= 0').sum("tpm")
+    @assists_sum = @stats.where('assists >= 0').sum("assists")
+    @steals_sum = @stats.where('steals >= 0').sum("steals")
+    @blocks_sum = @stats.where('blocks >= 0').sum("blocks")
+    @offReb_sum = @stats.where('"offReb" >= 0').sum("offReb")
+    @defReb_sum = @stats.where('"defReb" >= 0').sum("defReb")
+    @turnovers_sum = @stats.where('turnovers >= 0').sum("turnovers")
+    @pFouls_sum = @stats.where('"pFouls" >= 0').sum("pFouls")
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def per_game(value, game)
+    if game > 0
+    value.fdiv(game).round(1)
+    else
+      0
+    end
+  end
+
+  def for_value(value, total)
+    value.fdiv(total).round(4)
+  end
 end
