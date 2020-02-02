98cf48fc3607f21e42c1e7e9ead3f62cb6eb824e
apa yu <rx836@hotmail.com>
2020-02-02 21:53:52 +0800

    調整z-scores隨著數字大小變色

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,71 @@ require("bootstrap")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+window.onload = function () {
+
+  var player_value = document.getElementsByClassName("pa-value");
+  var max;
+  var min = 3
+  var arr = [];
+
+  for (var v_index = 0,td_length = player_value.length; v_index < td_length; v_index++)
+  {
+    arr[arr.length] = player_value[v_index].innerHTML;
+  }
+
+  max = Math.max.apply(null, arr);
+  min = Math.min.apply(null, arr);
+
+  for (var v_index = 0,td_length = player_value.length; v_index < td_length; v_index++)
+  {
+    var item_value = player_value[v_index].innerHTML
+    let c = getColorByNumber(item_value,max,min)
+    player_value[v_index].style.backgroundColor = c;
+  }
+
+
+  function rgbaToHex(color) {
+      var values = color
+        .replace(/rgba?\(/, '')
+        .replace(/\)/, '')
+        .replace(/[\s+]/g, '')
+        .split(',');
+      var a = parseFloat(values[3] || 1),
+        r = Math.floor(a * parseInt(values[0]) + (1 - a) * 255),
+        g = Math.floor(a * parseInt(values[1]) + (1 - a) * 255),
+        b = Math.floor(a * parseInt(values[2]) + (1 - a) * 255);
+
+      return "#" +
+        ("0" + r.toString(16)).slice(-2) +
+        ("0" + g.toString(16)).slice(-2) +
+        ("0" + b.toString(16)).slice(-2);
+  }
+
+  function getColorByNumber(n, max, min) {
+
+      var r = 255;
+      var g = 255;
+      var b = 255;
+
+    if(n < 0) {
+      var one = (-1 * n)/( -1 * min);
+      r = 255;
+      g = 255 - (255 * one);
+      b = 255 - (255 * one);
+    }
+
+    if(n > 0) {
+      var two = n/max;
+      r = 255 - (255 * two);
+      g = 255;
+      b = 255 - (255 * two);
+    }
+
+      r = parseInt(r);// 取整
+      g = parseInt(g);// 取整
+      b = parseInt(b);// 取整
+
+      return rgbaToHex("rgb(" + r + "," + g + "," + b + ")");
+  }
+}
