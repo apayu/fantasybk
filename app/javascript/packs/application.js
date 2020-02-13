53a4c9c91206eed7db07535f3797177fa8a6ab4d
apa yu <rx836@hotmail.com>
2020-02-13 19:58:01 +0800

    拿掉Turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -19,7 +18,6 @@ require("bootstrap")
 // const imagePath = (name) => images(name, true)
 
 window.onload = function () {
-
   var player_value = document.getElementsByClassName("pa-value");
   var max;
   var min = 3
