bc6f40c21cc4bb2efaa9ebe6853b77803e3a6584
apa yu <rx836@hotmail.com>
2020-02-20 19:46:16 +0800

    戰力表 v1.1 優化

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -15,9 +15,9 @@ class Table extends React.Component {
       // 聯盟隊伍數量
       league_num_teams: 0,
       // 聯盟開始週數
-      league_start_week: 1,
+      league_start_week: "1",
       // 聯盟目前週數
-      league_current_week: 1,
+      league_current_week: "1",
       // 數據板
       scoreboard: [],
       // 數據板換算得分
@@ -44,77 +44,65 @@ class Table extends React.Component {
         fetchInProgress: false}))
   }
 
-  // renderTableHeader() {
-  //   let header = Object.keys(this.state.scoreboard[0])
-  //   return header.map((key, index) => {
-  //     return <th key={index}>{key.toUpperCase()}</th>
-  //   })
-  // }
-
   // 數值越大，分數越高
-  sortScoreboard_asc(arr, num) {
+  sortScoreboard(arr, num, to_sort) {
     let newArray = arr.concat(num)
-    newArray.sort((a,b) => parseFloat(a)-parseFloat(b))
+
+    if(to_sort == "asc")
+      newArray.sort((a,b) => parseFloat(a)-parseFloat(b))
+    else
+      newArray.sort((a,b) => parseFloat(b)-parseFloat(a))
+
     return newArray.indexOf(num) + 1
-  }
-
-  // 數值越大，分數越小
-  sortScoreboard_desc(arr, num) {
-    let newArray = arr.concat(num)
-    newArray.sort((a,b) => parseFloat(b)-parseFloat(a))
-    return newArray.indexOf(num) + 1
-  }
-
-  renderTableDateByValue() {
-    // 選擇要秀出的 week 成績
-    let select_week = this.state.scoreboard.filter(x => x.week === "16")
-    let scoreboard_value = select_week
-    let ss = 0
-    for(let i = 3; i <= 13; i++) {
-      let total_array = select_week.map(x => Object.values(x)[i])
-      select_week.map(x=> {
-        let value_item  = Object.keys(x)[i]
-        let c = scoreboard_value.filter( y => y.id === x.id)
-
-        if(value_item.toLowerCase() == "to" || value_item.toLowerCase() == "pf")
-          ss = this.sortScoreboard_desc(total_array, Object.values(x)[i])
-        else
-          ss = this.sortScoreboard_asc(total_array, Object.values(x)[i])
-        // 尋找對應的隊伍計分板
-        c[0][value_item] = ss
-      })
-    }
-
-    return scoreboard_value.map((team, index) => {
-      const {name, id, gp, fg, ft, tpm, pts, oreb, dreb, ast, st, blk, to, pf} = team
-      return(
-        <tr key={index}>
-          <td scope="col">{name}</td>
-          <td scope="col">0</td>
-          <td scope="col">{fg}</td>
-          <td scope="col">{ft}</td>
-          <td scope="col">{tpm}</td>
-          <td scope="col">{pts}</td>
-          <td scope="col">{oreb}</td>
-          <td scope="col">{dreb}</td>
-          <td scope="col">{ast}</td>
-          <td scope="col">{st}</td>
-          <td scope="col">{blk}</td>
-          <td scope="col">{to}</td>
-          <td scope="col">{pf}</td>
-        </tr>
-      )
-    })
   }
 
   renderTableData() {
     // 選擇要秀出的 week 成績
-    let select_week = this.state.scoreboard.filter(x => x.week === "16")
-    return select_week.map((team, index) => {
+    let select_week_scoreboard = this.state.scoreboard.filter(x => x.week === this.state.league_current_week)
+    // 建立計分板
+    let scoreboard_value = JSON.parse(JSON.stringify(select_week_scoreboard))
+    let single_value = 0
+
+    // 算出各個項目的成績
+    // 從每一隊的第一個比項開始算
+    for(let i = 3; i <= 13; i++) {
+      let total_array = select_week_scoreboard.map(x => Object.values(x)[i])
+      let total_value = 0
+
+      // x等於各隊數據
+      select_week_scoreboard.map(x=> {
+        let value_item  = Object.keys(x)[i]
+        let c = scoreboard_value.filter( y => y.id == x.id)
+
+        //取得分數
+        if(value_item.toLowerCase() == "to" || value_item.toLowerCase() == "pf")
+          single_value = this.sortScoreboard(total_array, Object.values(x)[i], "desc")
+        else
+          single_value = this.sortScoreboard(total_array, Object.values(x)[i], "asc")
+
+        // 尋找對應的隊伍計分板
+        c[0][value_item] = single_value
+
+        // 加總到total_value
+        if (c && c[0]["total_value"]) {
+          c[0]["total_value"] = c[0]["total_value"] + single_value
+        }
+        else{
+          c[0]["total_value"] = single_value
+        }
+      })
+    }
+
+    return select_week_scoreboard.map((team, index) => {
+      // 從對應計分板尋找總分
+      let h = scoreboard_value.filter( y => y.id == team.id)
+
       const {name, id, gp, fg, ft, tpm, pts, oreb, dreb, ast, st, blk, to, pf} = team
+
       return(
         <tr key={index}>
           <td scope="col">{name}</td>
+          <td scope="col">{h[0].total_value}</td>
           <td scope="col">0</td>
           <td scope="col">{fg}</td>
           <td scope="col">{ft}</td>
@@ -143,6 +131,7 @@ class Table extends React.Component {
           <tbody className="thead-dark">
             <tr>
               <th>隊伍名稱</th>
+              <th>戰力值</th>
               <th>出場數(GP)</th>
               <th>FG%</th>
               <th>FT%</th>
@@ -163,37 +152,6 @@ class Table extends React.Component {
     )
   }
 
-  renderScoreboardValue() {
-    const league_name = this.state.league_name
-    const league_current_week = this.state.league_current_week
-
-    return (
-      <div>
-        <h1>{league_name} week {league_current_week} 戰力表</h1>
-        <table className="table">
-          <tbody className="thead-dark">
-            <tr>
-              <th>隊伍名稱</th>
-              <th>出場數(GP)</th>
-              <th>FG%</th>
-              <th>FT%</th>
-              <th>3PTM</th>
-              <th>PTS</th>
-              <th>OREB</th>
-              <th>DREB</th>
-              <th>AST</th>
-              <th>ST</th>
-              <th>BLK</th>
-              <th>TO</th>
-              <th>PF</th>
-            </tr>
-            {this.renderTableDateByValue()}
-          </tbody>
-        </table>
-      </div>
-    )
-  }
-
   renderNoToken() {
       <div>
         取得Yahoo Fantasy 授權
@@ -204,12 +162,7 @@ class Table extends React.Component {
     if(!this.state.fetchInProgress){
       return (
         <div>
-          <div>
-              {this.state.scoreboard.length > 0 ? this.renderScoreboard() : this.renderNoToken()}
-          </div>
-          <div>
-              {this.state.scoreboard.length > 0 ? this.renderScoreboardValue() : this.renderNoToken()}
-          </div>
+            {this.state.scoreboard.length > 0 ? this.renderScoreboard() : this.renderNoToken()}
         </div>
       )
     }
