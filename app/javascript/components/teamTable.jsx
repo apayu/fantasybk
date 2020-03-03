8b410c96f624f797bc40aa32a88773541b129bda
apa yu <rx836@hotmail.com>
2020-03-03 16:40:28 +0800

    fix 球隊分析 顏色

--- app/javascript/components/teamTable.jsx
+++ app/javascript/components/teamTable.jsx
@@ -6,9 +6,57 @@ class TeamTable extends React.Component {
     super(props)
   }
 
+  rgbaToHex(color) {
+      const values = color
+        .replace(/rgba?\(/, '')
+        .replace(/\)/, '')
+        .replace(/[\s+]/g, '')
+        .split(',')
+      const colorValue = parseFloat(values[3] || 1),
+        red = Math.floor(colorValue * parseInt(values[0]) + (1 - colorValue) * 255),
+        green = Math.floor(colorValue * parseInt(values[1]) + (1 - colorValue) * 255),
+        blue = Math.floor(colorValue * parseInt(values[2]) + (1 - colorValue) * 255)
+
+      return "#" +
+        ("0" + red.toString(16)).slice(-2) +
+        ("0" + green.toString(16)).slice(-2) +
+        ("0" + blue.toString(16)).slice(-2)
+  }
+
+  getColorByNumber(score, max, min) {
+
+      let red = 255
+      let green = 255
+      let blue = 255
+
+    if(score < 0) {
+      score = score < min ? -3 : score
+      var one = (-1 * score)/( -1 * min)
+      red = 255
+      green = 255 - (255 * one)
+      blue = 255 - (255 * one)
+    }
+
+    if(score > 0) {
+      score = score > max ? 5 : score
+      var two = score/max
+      red = 255 - (255 * two)
+      green = 255
+      blue = 255 - (255 * two)
+    }
+
+      red = parseInt(red)// 取整
+      green = parseInt(green)// 取整
+      blue = parseInt(blue)// 取整
+
+      return this.rgbaToHex("rgb(" + red + "," + green + "," + blue + ")")
+  }
+
   calculation(teamRoster, item) {
     let value = 0
     let isNotInj = 0
+    let avgValue = 0
+
     teamRoster.forEach(player =>{
       if(player['inj'] != true)
       {
@@ -16,8 +64,15 @@ class TeamTable extends React.Component {
         value += parseFloat(player[item])
       }
     })
+    avgValue = (value/isNotInj).toFixed(2)
+    const color = this.getColorByNumber(avgValue, 3, -3)
+    return(<td style={{backgroundColor: color}}>{avgValue}</td>)
+  }
 
-    return (value/isNotInj).toFixed(2)
+  addColor(value) {
+    const ValueFloat = parseFloat(value).toFixed(2)
+    const color = this.getColorByNumber(ValueFloat, 5, -3)
+    return(<td style={{backgroundColor: color}}>{ValueFloat}</td>)
   }
 
 
@@ -41,18 +96,18 @@ class TeamTable extends React.Component {
                 teams.map(team=>{
                   return(<tr key={team.teamId}>
                       <th scope="row">{team.teamName}</th>
-                      <td>{this.calculation(team.teamRoster, 'field_goal_value')}</td>
-                      <td>{this.calculation(team.teamRoster, 'free_throw_value')}</td>
-                      <td>{this.calculation(team.teamRoster, 'three_point_value')}</td>
-                      <td>{this.calculation(team.teamRoster, 'points_value')}</td>
-                      <td>{this.calculation(team.teamRoster, 'off_reb_value')}</td>
-                      <td>{this.calculation(team.teamRoster, 'def_reb_value')}</td>
-                      <td>{this.calculation(team.teamRoster, 'tot_reb_value')}</td>
-                      <td>{this.calculation(team.teamRoster, 'assists_value')}</td>
-                      <td>{this.calculation(team.teamRoster, 'steals_value')}</td>
-                      <td>{this.calculation(team.teamRoster, 'blocks_value')}</td>
-                      <td>{this.calculation(team.teamRoster, 'turnovers_value')}</td>
-                      <td>{this.calculation(team.teamRoster, 'p_fouls_value')}</td>
+                      {this.calculation(team.teamRoster, 'field_goal_value')}
+                      {this.calculation(team.teamRoster, 'free_throw_value')}
+                      {this.calculation(team.teamRoster, 'three_point_value')}
+                      {this.calculation(team.teamRoster, 'points_value')}
+                      {this.calculation(team.teamRoster, 'off_reb_value')}
+                      {this.calculation(team.teamRoster, 'def_reb_value')}
+                      {this.calculation(team.teamRoster, 'tot_reb_value')}
+                      {this.calculation(team.teamRoster, 'assists_value')}
+                      {this.calculation(team.teamRoster, 'steals_value')}
+                      {this.calculation(team.teamRoster, 'blocks_value')}
+                      {this.calculation(team.teamRoster, 'turnovers_value')}
+                      {this.calculation(team.teamRoster, 'p_fouls_value')}
                     </tr>)
                 })
               }
@@ -85,18 +140,18 @@ class TeamTable extends React.Component {
                       <td>{player.tricode}</td>
                       <td>{player.g}</td>
                       <td>{player.min.slice(3,8)}</td>
-                      <td>{parseFloat(player.field_goal_value).toFixed(2)}</td>
-                      <td>{parseFloat(player.free_throw_value).toFixed(2)}</td>
-                      <td>{parseFloat(player.three_point_value).toFixed(2)}</td>
-                      <td>{parseFloat(player.points_value).toFixed(2)}</td>
-                      <td>{parseFloat(player.off_reb_value).toFixed(2)}</td>
-                      <td>{parseFloat(player.def_reb_value).toFixed(2)}</td>
-                      <td>{parseFloat(player.tot_reb_value).toFixed(2)}</td>
-                      <td>{parseFloat(player.assists_value).toFixed(2)}</td>
-                      <td>{parseFloat(player.steals_value).toFixed(2)}</td>
-                      <td>{parseFloat(player.blocks_value).toFixed(2)}</td>
-                      <td>{parseFloat(player.turnovers_value).toFixed(2)}</td>
-                      <td>{parseFloat(player.p_fouls_value).toFixed(2)}</td>
+                      {this.addColor(player.field_goal_value)}
+                      {this.addColor(player.free_throw_value)}
+                      {this.addColor(player.three_point_value)}
+                      {this.addColor(player.points_value)}
+                      {this.addColor(player.off_reb_value)}
+                      {this.addColor(player.def_reb_value)}
+                      {this.addColor(player.tot_reb_value)}
+                      {this.addColor(player.assists_value)}
+                      {this.addColor(player.steals_value)}
+                      {this.addColor(player.blocks_value)}
+                      {this.addColor(player.turnovers_value)}
+                      {this.addColor(player.p_fouls_value)}
                       <td>{player.rank}</td>
                     </tr>)
                 })
